facc18d8fad2453b3c9591a3678875c24e1c7836
patelprakash0508 <patelprakash0508@gmail.com>
2022-05-28 23:06:58 +0200

    upadated js file according to feedback

--- javascript/javascript1/week1/myscripts.js
+++ javascript/javascript1/week1/myscripts.js
@@ -2,49 +2,46 @@
 const yearOfBirth = 1987;
 const yearFuture = 2027;
 const age = yearFuture - yearOfBirth;
-console.log("You will be " + age + " years old in " + yearFuture);
+console.log(`You will be ${age} years old in  ${yearFuture}`);
 
 // Task 2
-let dogYearOfBirth = 2017;
-let dogYearFuture = 2027;
-let dogYear = dogYearFuture - dogYearOfBirth + 60;
-let shouldShowResultInDogYear = true;
+const dogYearOfBirth = 2017;
+const dogYearFuture = 2027;
+const dogYear = (dogYearFuture - dogYearOfBirth) * 7;
+const shouldShowResultInDogYear = true;
 
-if (shouldShowResultInDogYear == true) {
-  console.log(
-    "Your dog will be " + dogYear + " dog years old in " + dogYearFuture
-  );
+if (shouldShowResultInDogYear) {
+  console.log(`Your dog will be  ${dogYear} dog years old in ${dogYearFuture}`);
 } else {
-  let humanYear = dogYear - 60;
+  const humanYear = dogYear / 7;
   console.log(
-    "Your dog will be " + humanYear + " human years old in " + dogYearFuture
+    `Your dog will be ${humanYear} human years old in ${dogYearFuture}`
   );
-};
+}
 
 // Task 3
 // Peter house prise calculation
-let peter = { wide: 8, deep: 10, high: 10, gardenSizeInM2: 100 };
-let peterVolumeInMeters = peter.wide * peter.deep * peter.high;
-let peterHousePrise =
+const peter = { wide: 8, deep: 10, high: 10, gardenSizeInM2: 100 };
+const peterVolumeInMeters = peter.wide * peter.deep * peter.high;
+const peterHousePrice =
   peterVolumeInMeters * 2.5 * 1000 + peter.gardenSizeInM2 * 300;
-if (peterHousePrise > 2500000) {
+if (peterHousePrice >= 2500000) {
   console.log("Peter is paying too much");
 } else {
   console.log("Peter is paying too little");
-};
+}
 
-// Julia house prise calculation 
-let julia = { wide: 5, deep: 11, high: 8, gardenSizeInM2: 70 };
-let juliaVolumeInMeters = julia.wide * julia.deep * julia.high;
-let juliaHousePrise =
+// Julia house price calculation
+const julia = { wide: 5, deep: 11, high: 8, gardenSizeInM2: 70 };
+const juliaVolumeInMeters = julia.wide * julia.deep * julia.high;
+const juliaHousePrice =
   juliaVolumeInMeters * 2.5 * 1000 + julia.gardenSizeInM2 * 300;
 
-if (juliaHousePrise > 1000000) {
+if (juliaHousePrice >= 1000000) {
   console.log("Julia is paying too much");
 } else {
   console.log("Julia is paying too little");
-};
-
+}
 
 // Task 4
 const firstWords = [
@@ -71,11 +68,10 @@ const secondWords = [
   "Cross",
   "Serva",
 ];
-const randomNumber = Math.floor(Math.random() * 10);
-const startupName = firstWords[randomNumber] + secondWords[randomNumber];
+const randomNumber1 = Math.floor(Math.random() * 10);
+const randomNumber2 = Math.floor(Math.random() * 10);
+
+const startupName = firstWords[randomNumber1] + secondWords[randomNumber2];
 console.log(
-  "New Startup Name is " +
-    startupName +
-    " Number of characters are " +
-    startupName.length
+  `New Startup Name is ${startupName} Number of characters are ${startupName.length}`
 );

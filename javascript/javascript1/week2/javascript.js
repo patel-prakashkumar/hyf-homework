9ed99a44ac26741ec376645cce78ea2f292877c9
patelprakash0508 <patelprakash0508@gmail.com>
2022-06-06 23:27:58 +0200

    I have resolved issue tast 1 to 4

--- javascript/javascript1/week2/javascript.js
+++ javascript/javascript1/week2/javascript.js
@@ -1,9 +1,10 @@
 // Task 1: Add two parameter in function.
 
 function getFullName(firstname, lastname) {
-    console.log(`"${firstname} ${lastname}"`);
+    return `${firstname} ${lastname}`;
 }
-getFullName("Benjamin", "Hughes");
+console.log(getFullName("Benjamin", "Hughes"));
+
 
 
 // fullname with formalname and gender condition check , also checked empty string in firstname and lastname
@@ -14,37 +15,65 @@ const fullName3 = getFullName("Maahit", "Patel", "male", false);
 const fullName4 = getFullName("Preshu", "Patel", "female", true);
 const fullName5 = getFullName("", "", "female", true);
 const fullName6 = getFullName("Prakash", "Patel", "male", true);
+const fullName7 = getFullName(" ", " ", "female", true);
+const fullName8 = getFullName(true, true, "female", true);
 
 function getFullName(firstname, lastname, gender, useFormalName) {
-    if (firstname === "" || lastname === "") {
+    if (firstname.length === 0 || lastname.length === 0) {
         console.warn("please enter first name or last name")
     }
-    else {
-        if (useFormalName == true) {
-            if (gender == "male") {
-                console.log(`"Lord ${firstname} ${lastname}"`);
-            }
-            else
-                console.log(`"${firstname} ${lastname}"`)
-        } else {
-            console.log(`"${firstname} ${lastname}"`);
+    else if (typeof (firstname) !== 'string' || typeof (lastname) !== 'string') {
+        console.warn("please enter correct first name or last name")
+    }
+
+    else if (firstname.match(/^\s+$/) !== null || lastname.match(/^\s+$/) !== null) {
+        console.warn("please remove empty space and enter correct first name or last name")
+    }
+
+    else if (useFormalName === true) {
+        if (gender == "male") {
+            return `Lord ${firstname} ${lastname}`;
+        }
+        else {
+            return `Lady ${firstname} ${lastname}`;
         }
     }
+
+    else {
+
+        return `${firstname} ${lastname}`;
+    }
+
+
 }
-getFullName(fullName1);
+
+console.log(fullName1);
+console.log(fullName2);
+console.log(fullName3);
+console.log(fullName4);
+console.log(fullName5);
+console.log(fullName6);
+console.log(fullName7);
+console.log(fullName8);
+
 
 
 // Task 2 Eventday calculator 
 
-const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-const weekDay = new Date();
+
 
 function getEventWeekday(eventday) {
+    const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    const weekDay = new Date();
 
     weekDay.setDate(weekDay.getDate() + eventday);
     return weekDays[weekDay.getDay()];
 }
-console.log(`Event held on ${getEventWeekday(6)}`);
+getEventWeekday(1);
+getEventWeekday(1);
+console.log(`Event held on ${getEventWeekday(1)}`);
+
+
 
 // Task 3  Weather to Wear cloth.
 
@@ -54,7 +83,7 @@ const summer = weatherToWear(20);
 
 function weatherToWear(temperture) {
     if (temperture <= 10) {
-        return "Sweter, Jacket & Warm Cap";
+        return "Sweater, Jacket & Warm Cap";
     } else if (temperture <= 18) {
         return "Fullsleve T-shirt & Jense";
     } else {
@@ -68,33 +97,58 @@ console.log(spring);
 console.log(summer);
 
 // Task 4 Student Manager 
+
 const class07Students = [];
 function addStudentToClass(studentName) {
     // You write code here
 
-    if (class07Students.length > 6) {
-        if (studentName == "Queen") {
-            class07Students.push(studentName);
-        } else {
-            console.log("Cannot add more students to class");
+    if (typeof (studentName) !== 'string' || studentName.match(/^\s+$/) !== null) {
+        console.warn("Please enter correct student name")
+    }
+    else if (class07Students.length > 6)
+        if (studentName === "Queen") {
+            if (class07Students.includes(studentName)) {
+                console.warn("Queen already in class");
+            }
+            else {
+
+                class07Students.push(studentName);
+            }
         }
-    } else {
+        else {
+
+            console.warn("Cannot add more students to class");
+
+        }
+
+    else {
         if (class07Students.includes(studentName)) {
-            console.log("Student is already in the class");
+            console.warn("Student is already in the class");
         } else {
             class07Students.push(studentName);
         }
     }
+
+
+
+
 }
+
+
 
 addStudentToClass("Prakash");
 addStudentToClass("Prakash");
 addStudentToClass("Michel");
 addStudentToClass("Danial");
-addStudentToClass("Jakub");
-addStudentToClass("Milo");
 addStudentToClass("Tommy");
 addStudentToClass("Christian");
+addStudentToClass("Queen");
+addStudentToClass("Prakash");
+addStudentToClass();
+addStudentToClass(true);
+addStudentToClass(null);
+addStudentToClass("");
+addStudentToClass(" ");
 addStudentToClass("Queen");
 addStudentToClass("Prakash");
 
@@ -132,6 +186,7 @@ function addCandy(candyType, weight) {
         boughtCandyPrices.push(weight * 0.03);
     }
 
+
     return boughtCandyPrices;
 
 }
@@ -140,6 +195,8 @@ addCandy("Chocolate", 25);
 addCandy("Toffe", 15);
 addCandy("Chewing-gum", 40);
 console.log(boughtCandyPrices);
+
+
 
 // create function canBuyMoreCandy and match total price with math.random()
 const amountToSpend = Math.random() * 100;

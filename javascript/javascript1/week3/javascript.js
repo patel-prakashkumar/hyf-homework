5df71fccf5f5864c82ddb99ab8a2fc858dee0bc0
patelprakash0508 <patelprakash0508@gmail.com>
2022-06-15 22:50:34 +0200

    add CactusIO-interactive (Smart phone usage app) task

--- javascript/javascript1/week3/javascript.js
+++ javascript/javascript1/week3/javascript.js
@@ -62,11 +62,13 @@ function logOutSeriesText() {
     // write code here
     let totalPerLifeSpan = 0;
     const age = 80;
+    let i = 1
     for (let i = 0; i < seriesDurations.length; i++) {
         const serialTime = seriesDurations[i].days + (seriesDurations[i].hours / 24);
         const lifeLongSpan = age * 365;
         const perLifeSpan = serialTime * 100 / lifeLongSpan;
-        console.log(`${seriesDurations[i].title} took ${perLifeSpan.toFixed(3)}% of my life`)
+        
+        //console.log(`${seriesDurations[i].title} took ${perLifeSpan.toFixed(3)}% of my life`)
         totalPerLifeSpan += +perLifeSpan.toFixed(3)
     }
     // Return total percentage of life Span 
@@ -124,10 +126,48 @@ console.log(firstNote);
 
 function logOutNotesFormatted() {
     // your code here
-    for (let i = 0; notes.length; i++) {
+    for (let i = 0; i < notes.length; i++) {
         console.log(`The note with id: ${notes[i].id}, has the following note text: ${notes[i].content}`)
     }
 }
 logOutNotesFormatted();
+
+// CactusIO-interactive (Smart phone usage app) optional
+let dailyActivities = [];
+function addActivity(date, name, duration) {
+    if (typeof (date) !== `string` || typeof (name) !== 'string' || typeof (duration) !== 'number') {
+        console.warn("Please enter valid Input")
+    }
+    else {
+        let activities = {
+            date: date,
+            name: name,
+            duration: duration
+        }
+        dailyActivities.push(activities)
+    }
+    return dailyActivities;
+}
+addActivity("23/7-18", "Youtube", 30);
+addActivity("24/7-18", "Facebook", 45);
+addActivity("25/7-18", "Reading book", 60);
+addActivity("26/7-18", "Surfing on Internet", 35);
+console.log(dailyActivities)
+
+function showStatus() {
+    let totalDuration = 0;
+    for (let i = 0; i < dailyActivities.length; i++) {
+        totalDuration += dailyActivities[i].duration;
+    }
+    console.log(`Total duration of activities: ${totalDuration} min.`)
+    if (dailyActivities.length == 0) {
+        return console.log("Add some activities before calling showStatus");
+    } else if (totalDuration < 175) {  //Usage limit
+        return console.log(`You have added ${dailyActivities.length} activities They amount to ${totalDuration} min. of usage`);
+    } else {
+        return console.log("You have reached your limit, no more smartphoning for you!");
+    }
+}
+showStatus()
 
 

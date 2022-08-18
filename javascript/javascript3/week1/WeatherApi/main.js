2a76169b20dcd519c929f85d33ed22034451074a
Prakashkumar Patel <patelprakash0508@gmail.com>
2022-08-18 20:35:56 +0200

    Added google map and add rander function  in main.js file

--- javascript/javascript3/week1/WeatherApi/main.js
+++ javascript/javascript3/week1/WeatherApi/main.js
@@ -7,6 +7,7 @@ const bgImg = document.querySelector('div .dis-part1')
 const disIcon = document.querySelector('div .weather-Icon')
 const disSunTime = document.getElementById('sun-time')
 const gmtDateTime = new Date().toUTCString();
+const randerMap = document.getElementById('display-map')
 const apiKey = `aff81a037c13a860266157fd7cec79ed`
 let inputCityName = "copenhagen"
 btnSubmit.addEventListener('click', () => {
@@ -17,6 +18,38 @@ btnSubmit.addEventListener('click', () => {
     }
     display();
 });
+
+const render = (data) => {
+    disSunTime.innerHTML = `
+        <h6>Sunrise: ${data.sunriseGMT.toUTCString()}</h6>
+        <h6>Sunset: ${data.sunsetGMT.toUTCString()}</h6>
+    `
+    disTemp.innerHTML = `
+    <h2>Temperature</h2>
+    <h2>${Math.floor(data.temp)} °C</h2> 
+    <h2> ${Math.floor(data.fahrenheit)} °F</h2> 
+    <h2>Wind Speed</h2> 
+    <h3> ${Math.floor(data.speed)} m/s</h3>
+    `
+    disCityDetail.innerHTML = `
+    <h1>${data.cityName ?? "Current Location"}</h1
+    ><h6> ${gmtDateTime}</h6>
+    `
+    let imgIcon = document.createElement('img');
+    disIcon.innerHTML = `
+    <h3>${data.main}</h3>
+    `
+    disIcon.appendChild(imgIcon).setAttribute("src", data.iconUrl)
+    weatherDesUL.innerHTML = `
+    <li>Description: ${data.description}</li> 
+    <li>Feel like: ${Math.floor(data.feels_like)} °C</li> 
+    <li>Temp Min: ${Math.floor(data.temp_min)} °C</li> 
+    <li>Temp Max: ${Math.floor(data.temp_max)} °C</li> 
+    <li>Humidity: ${data.humidity}</li>
+    `
+    document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + inputCityName + ")"
+}
+
 const display = () => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCityName}&units=metric&appid=${apiKey}`)
         .then(response => response.json())
@@ -32,14 +65,9 @@ const display = () => {
             const { sunrise, sunset } = weatherData.sys;
             const sunriseGMT = new Date(sunrise * 1000);
             const sunsetGMT = new Date(sunset * 1000);
-            disSunTime.innerHTML = `<h6>Sunrise: ${sunriseGMT.toUTCString()}</h6><h6>Sunset: ${sunsetGMT.toUTCString()}</h6>`
-            disTemp.innerHTML = `<h2>Temperature</h2><h2>${Math.floor(temp)} °C</h2> <h2> ${Math.floor(fahrenheit)} °F</h2> <h2>Wind Speed</h2> <h3> ${Math.floor(speed)} m/s</h3>`
-            disCityDetail.innerHTML = `<h1>${cityName}</h1><h6> ${gmtDateTime}</h6  >`
-            let imgIcon = document.createElement('img');
-            disIcon.innerHTML = `<h3>${main}</h3>`
-            disIcon.appendChild(imgIcon).setAttribute("src", iconUrl)
-            weatherDesUL.innerHTML = `<li>Description: ${description}</li> <li>Feel like: ${Math.floor(feels_like)} °C</li> <li>Temp Min: ${Math.floor(temp_min)} °C</li> <li>Temp Max: ${Math.floor(temp_max)} °C</li> <li>Humidity: ${humidity}</li>`
-            document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + inputCityName + ")"
+            render({ sunriseGMT, sunsetGMT, temp, fahrenheit, speed, cityName, main, iconUrl, description, feels_like, temp_min, temp_max, humidity });
+            randerMap.innerHTML = `<iframe src = "https://maps.google.com/maps?q=${inputCityName}&hl=es;z=14&amp;output=embed" ></iframe>`
+
         });
 }
 inputCityName = "copenhagen"
@@ -67,13 +95,9 @@ btnLocation.addEventListener('click', function () {
                 const { sunrise, sunset } = weatherData.current;
                 const sunriseGMT = new Date(sunrise * 1000);
                 const sunsetGMT = new Date(sunset * 1000);
-                disSunTime.innerHTML = `<h6>Sunrise: ${sunriseGMT.toUTCString()}</h6><h6>Sunset: ${sunsetGMT.toUTCString()}</h6>`
-                disTemp.innerHTML = `<h3>Temperature</h3><h1>${Math.floor(temp)} °C</h1> <h1> ${Math.floor(fahrenheit)} °F</h1> <h3>Wind Speed</h3> <h1> ${Math.floor(wind_speed)} m/s</h1>`
-                disCityDetail.innerHTML = `<h1>Cuurent Location</h1><h6> ${gmtDateTime}</h6  >`
-                let imgIcon = document.createElement('img');
-                disIcon.innerHTML = `<h3>${main}</h3>`
-                disIcon.appendChild(imgIcon).setAttribute("src", iconUrl)
-                weatherDesUL.innerHTML = `<li>Description: ${description}</li> <li>Feel like: ${Math.floor(feels_like)} °C</li> <li>Temp Min: ${Math.floor(min)} °C</li> <li>Temp Max: ${Math.floor(max)} °C</li> <li>Humidity: ${humidity}</li>`
+                render({ sunriseGMT, sunsetGMT, temp, fahrenheit, speed: wind_speed, main, iconUrl, description, feels_like, temp_min: min, temp_max: max, humidity });
+                randerMap.innerHTML = `<iframe src = "https://maps.google.com/maps?q=${x},${y}&hl=es;z=14&amp;output=embed"></iframe>`
+
             });
     })
 }) 

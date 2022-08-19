66a8551f4543b406ac3d14f03cc8487d6b499b32
Prakashkumar Patel <patelprakash0508@gmail.com>
2022-08-19 12:58:31 +0200

    Update sql querries according to feedback

--- databases/week1/Assignment_Task_Queries.sql
+++ databases/week1/Assignment_Task_Queries.sql
@@ -1,85 +1,23 @@
+-- Active: 1660903523654@@127.0.0.1@3306@database_week1
 USE database_week1;
 -- Find out how many tasks are in the task table
-SELECT 
-    `title`
-FROM
-    `task`;
-
+SELECT `title` FROM `task`;
 -- Find out how many tasks in the task table do not have a valid due date
-SELECT 
-    `due_date`
-FROM
-    `task`
-WHERE
-    `due_date` IS NULL;
-
+SELECT `due_date` FROM `task` WHERE `due_date` IS NULL;
 -- Find all the tasks that are marked as done
-SELECT 
-    task.id, task.title, status.name
-FROM
-    `task`
-        INNER JOIN
-    `status` ON status.id = task.status_id
-WHERE
-    status.name = 'Done';
-
+SELECT task.id, task.title, status.name FROM `task` INNER JOIN `status` ON status.id = task.status_id WHERE status.name = 'Done';
 -- Find all the tasks that are not marked as done
-SELECT 
-    task.id, task.title, status.name
-FROM
-    `task`
-        INNER JOIN
-    `status` ON status.id = task.status_id
-WHERE
-    status.name != 'Done';
-
+SELECT task.id, task.title, status.name FROM `task` INNER JOIN `status` ON status.id = task.status_id WHERE status.name != 'Done';
 -- Get all the tasks, sorted with the most recently created first
-SELECT 
-    id, title, created
-FROM
-    `task`
-WHERE
-    `created`
-ORDER BY `created` DESC;
-
+SELECT id, title, created FROM `task` WHERE `created` ORDER BY `created` DESC;
 -- Get the single most recently created task
-SELECT 
-    id, title, MAX(created)
-FROM
-    `task`;
-
+SELECT id, title, MAX(created) FROM `task`;
 -- Get the title and due date of all tasks where the title or description contains database
-SELECT 
-    title, description
-FROM
-    `task`
-WHERE
-    title LIKE '%database%'
-        OR description LIKE '%database%';
-
+SELECT title, due_date FROM `task` WHERE title LIKE '%database%' OR description LIKE '%database%';
 -- Get the title and status (as text) of all tasks
-SELECT 
-    CONCAT(task.title, '  ', status.name) AS TEXT
-FROM
-    `task`
-        INNER JOIN
-    `status` ON status.id = task.status_id;
-
+SELECT CONCAT(task.title, '  ', status.name) AS TEXT FROM `task` INNER JOIN `status` ON status.id = task.status_id;
 -- Get the name of each status, along with a count of how many tasks have that status
-SELECT 
-    status.name, COUNT(task.title)
-FROM
-    task
-        INNER JOIN
-    `status` ON status.id = task.status_id
-GROUP BY status.name;
-
+SELECT status.name, COUNT(task.title) FROM task INNER JOIN `status` ON status.id = task.status_id GROUP BY status.name;
 -- Get the names of all statuses, sorted by the status with most tasks first
-SELECT 
-    status.name, task.title, task.updated
-FROM
-    `task`
-        INNER JOIN
-    `status` ON status.id = task.status_id
-GROUP BY task.title , status.name
-ORDER BY status.name AND task.updated DESC;
+-- SELECT status.name, task.title, task.updated FROM `task` INNER JOIN `status` ON status.id = task.status_id GROUP BY task.title, status.name  ORDER BY status.name AND task.updated DESC;
+SELECT status.name, COUNT(status.name) FROM  `task` INNER JOIN `status` ON status.id = task.status_id GROUP BY status.name ORDER BY COUNT(status.name)  DESC;

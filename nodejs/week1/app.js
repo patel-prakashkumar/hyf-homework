9ac4430da4207d2ee30c731e255bd5327c654973
Prakashkumar Patel <patelprakash0508@gmail.com>
2022-09-11 12:10:43 +0200

    upadet app.js file

--- nodejs/week1/app.js
+++ nodejs/week1/app.js
@@ -2,7 +2,11 @@
 // parse command line arguments
 const listNumber = process.argv.slice(2).map(num => Number(num));
 let sum = 0;
+listNumber.forEach(function (number) {
+  sum += number        
+});
 const average = sum / listNumber.length;
-sum > 0
+console.log(average)
+average > 0
   ? console.log(`Average Number is  ${average.toFixed(2)}`)
   : console.log('Enter Valid Input or Number');
